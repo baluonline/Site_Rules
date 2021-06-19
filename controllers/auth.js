4879a20ce369a1e8ac87377aa82822f1da309f89
Balaji <balurides@gmail.com>
2021-06-19 05:19:56 -0500

    Developed backendposts

---
+++ controllers/auth.js
@@ -0,0 +1,71 @@
+const { validationResult } = require('express-validator/check')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+exports.signup = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+        const error = new Error('Validation failed')
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error
+    }
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    bcrypt
+        .hash(password, 12)
+        .then(hashedPwd => {
+            const user = new User({
+                email: email,
+                password: hashedPwd,
+                name: name,
+                status: 'active'
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.status(201).json({ message: 'User created', userId: result._id })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+}
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email }).then(user => {
+        if (!user) {
+            const error = new Error("A user with this email could not be found");
+            error.statusCode = 401;
+            throw error;
+        }
+        loadUser = user;
+        return bcrypt.compare(password, user.password)
+    })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error("Wrong password");
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign({
+                email: loadUser.email,
+                userId: loadUser._id.toString()
+            }, 'somesupersecretsecret',
+                { expiresIn: '1h' })
+            res.status(200).json({token:token,userId:loadUser._id.toString()})
+        })
+
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+}
---
+++ routes/auth.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user')
+const authController = require('../controllers/auth')
+const { body } = require('express-validator/check');
+
+router.put('/signup', [
+    body('email').isEmail().withMessage('Please enter valid email address')
+        .custom((value, { req }) => {
+            return User.findOne({ email: value }).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('E-mail address is alread exists')
+                }
+            })
+        })
+        .normalizeEmail(),
+    body('password').trim().isLength({ min: 5 }),
+    body('name').trim().not().isEmpty()
+], authController.signup)
+
+router.put('/login', authController.login)
+
+module.exports = router;
